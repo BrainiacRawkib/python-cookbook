198769133e794d015592bf35687b947140dbcc74
Br@ini@c R@wkib <brainiacrawkib@gmail.com>
2021-10-07 02:51:49 -0700

    :tada: chapter nine

--- src/ch_09/ex_22b.py
+++ src/ch_09/ex_22b.py
@@ -7,3 +7,16 @@ def list_transaction(orig_list):
     working = list(orig_list)
     yield working
     orig_list[:] = working
+
+
+items = [1, 2, 3, 4]
+
+with list_transaction(items) as working:
+    working.append(5)
+    working.append(6)
+    working.append(7)
+
+print(items)
+
+if __name__ == '__main__':
+    pass
